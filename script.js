7cb6fe6796d977a5a080ef2cde324d73817208ea
Stephen Whitted <stephent.whitted@gmail.com>
2024-04-19 22:09:15 -0400

    part 2

--- script.js
+++ script.js
@@ -92,5 +92,47 @@ console.log('Processed Problematic Data:', processedProblematicData);
 
 
 ////////////////////Part 2/////////////////////////////////////////////
+/*
+Now that you are familiar with your code, and perhaps have improved it, it is time to expand upon its functionality.
+Begin with the following task:
+Declare a variable that stores the number of columns in each row of data within the CSV.
+Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+For example, if the first row of data (the headings) has eight entries, your program should create eight entries per row. You can safely assume that all rows that follow will contain the same number of entries per row.
+After you have implemented the above:
+Store your results in a two-dimensional array.
+Each row should be its own array, with individual entries for each column.
+Each row should be stored in a parent array, with the heading row located at index 0.
+Cache this two-dimensional array in a variable for later use.
+Using the original CSV example data, here is what the result of this step should look like:
+ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+becomes
+[["ID", "Name", "Occupation", "Age"], ["42", "Bruce", "Knight", "41"], ["57", "Bob", "Fry Cook", "19"], ["63", "Blaine", "Quiz Master", "58"], ["98", "Bill", "Doctor’s Assistant", "26"]]  */
+
+// CSV data
+const csvData2 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+function parseCSV2(csv2String) {
+  
+    const rows = csv2String.split("\n");
+
+    const parsed2Data = [];
+
+    for (let i = 0; i < rows.length; i++) {
+       
+        const columns = rows[i].split(",");
+
+        
+        parsed2Data.push(columns);
+    }
+
+ 
+    return parsed2Data;
+}
+
+
+const result = parseCSV2(csvData2);
+console.log(result);
+
+/////////////////Part 3////////////////////////////////////////////
 
 
